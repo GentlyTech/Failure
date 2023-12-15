888300982c898615421138900a8b3469bc9d892e
GentlyTech <andyyep@hotmail.com>
2023-12-14 22:23:53 -0500

    Initial Commit

---
+++ app/src/main/java/com/yepdevelopment/failure/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yepdevelopment.failure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yepdevelopment.failure.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
