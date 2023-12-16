4d778772f2d458a3e37f66e6c0ea816d577c658c
GentlyTech <andyyep@hotmail.com>
2023-12-16 00:03:03 -0500

    Preliminary nav implementation/experimentation

--- app/src/main/java/com/yepdevelopment/failure/Activities/MainActivity.java
+++ app/src/main/java/com/yepdevelopment/failure/Activities/MainActivity.java
@@ -1,13 +1,16 @@
 package com.yepdevelopment.failure.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.yepdevelopment.failure.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/yepdevelopment/failure/Fragments/HomeFragment.java
+++ app/src/main/java/com/yepdevelopment/failure/Fragments/HomeFragment.java
@@ -2,23 +2,39 @@ package com.yepdevelopment.failure.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.yepdevelopment.failure.R;
 
 public class HomeFragment extends Fragment {
+        NavController navController;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        navController = NavHostFragment.findNavController(this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        FloatingActionButton floatingActionButtonAddCourse = view.findViewById(R.id.floatingActionButtonAddCourse);
+
+        floatingActionButtonAddCourse.setOnClickListener(ign -> navController.navigate(HomeFragmentDirections.actionHomeFragmentToAddCourseFragment()));
     }
 }
