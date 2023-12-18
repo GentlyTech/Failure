2d4cb842db0aa077e350983cd2eedb5acf90c213
GentlyTech <andyyep@hotmail.com>
2023-12-17 21:05:43 -0500

    Add annotation to CourseDAO

--- app/src/main/java/com/yepdevelopment/failure/Database/DAOs/CourseDAO.java
+++ app/src/main/java/com/yepdevelopment/failure/Database/DAOs/CourseDAO.java
@@ -1,5 +1,6 @@
 package com.yepdevelopment.failure.Database.DAOs;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
@@ -8,6 +9,7 @@ import com.yepdevelopment.failure.Database.Entities.Course;
 
 import java.util.List;
 
+@Dao
 public interface CourseDAO {
     @Query("SELECT * FROM course")
     List<Course> getAll();
