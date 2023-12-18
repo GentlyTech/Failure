ec1c17ffe59e179c9d8548ba9a76b4ad9d18209e
GentlyTech <andyyep@hotmail.com>
2023-12-18 18:25:18 -0500

    Add conditional rendering to course list

--- app/src/main/java/com/yepdevelopment/failure/Fragments/HomeFragment.java
+++ app/src/main/java/com/yepdevelopment/failure/Fragments/HomeFragment.java
@@ -7,14 +7,19 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.Group;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.yepdevelopment.failure.Database.Entities.Course;
 import com.yepdevelopment.failure.R;
 import com.yepdevelopment.failure.ViewModels.MainViewModel;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
     NavController navController;
@@ -36,7 +41,21 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         FloatingActionButton floatingActionButtonAddCourse = view.findViewById(R.id.floatingActionButtonAddCourse);
+        Group groupCourseListEmptyText = view.findViewById(R.id.groupCourseListEmptyText);
+        RecyclerView recyclerViewCourseList = view.findViewById(R.id.recyclerViewCourseList);
 
-        floatingActionButtonAddCourse.setOnClickListener(ign -> navController.navigate(HomeFragmentDirections.actionHomeFragmentToAddCourseFragment()));
+        List<Course> courses = mainViewModel.getCourses().getValue();
+        if (courses != null) {
+            if (courses.isEmpty()) {
+                groupCourseListEmptyText.setVisibility(View.VISIBLE);
+                recyclerViewCourseList.setVisibility(View.GONE);
+            } else {
+                groupCourseListEmptyText.setVisibility(View.GONE);
+                recyclerViewCourseList.setVisibility(View.VISIBLE);
+            }
+        }
+
+        floatingActionButtonAddCourse.setOnClickListener(ignored -> navController.navigate(HomeFragmentDirections.actionHomeFragmentToAddCourseFragment()));
     }
+
 }
