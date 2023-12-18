8f334598ce5717fc6b53e508a7fe30e3c7c78b40
GentlyTech <andyyep@hotmail.com>
2023-12-18 17:58:09 -0500

    Set up bindings for AddCourseFragment Set cancel button onClick listener

--- app/src/main/java/com/yepdevelopment/failure/Fragments/AddCourseFragment.java
+++ app/src/main/java/com/yepdevelopment/failure/Fragments/AddCourseFragment.java
@@ -4,32 +4,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.yepdevelopment.failure.R;
 import com.yepdevelopment.failure.ViewModels.MainViewModel;
+import com.yepdevelopment.failure.databinding.FragmentAddCourseBinding;
 
 public class AddCourseFragment extends Fragment {
     MainViewModel mainViewModel;
+    NavController navController;
+    private FragmentAddCourseBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainViewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
+        navController = NavHostFragment.findNavController(this);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_course, container, false);
+        binding = FragmentAddCourseBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Button buttonCancelAddCourse = view.findViewById(R.id.buttonCancelAddCourse);
+        Button buttonConfirmAddCourse = view.findViewById(R.id.buttonConfirmAddCourse);
+        TextInputLayout editTextLayoutCourseStartDate = view.findViewById(R.id.editTextLayoutCourseStartDate);
+        TextInputLayout editTextLayoutCourseEndDate = view.findViewById(R.id.editTextLayoutCourseEndDate);
 
+        buttonCancelAddCourse.setOnClickListener(button -> navController.popBackStack());
     }
 }
