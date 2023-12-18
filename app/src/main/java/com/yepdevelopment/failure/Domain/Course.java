95ab2e2d34c33ccdd3e5fea8de9996347760ec94
GentlyTech <andyyep@hotmail.com>
2023-12-17 20:42:17 -0500

    Add placeholder constructors Fix accessors Reformat files

--- app/src/main/java/com/yepdevelopment/failure/Domain/Course.java
+++ app/src/main/java/com/yepdevelopment/failure/Domain/Course.java
@@ -7,117 +7,73 @@ import java.util.List;
  * @generated
  */
 public class Course {
-
-    /**
-     * @generated
-     */
+    private String id;
     private String name;
-
-    /**
-     * @generated
-     */
     private String subject;
-
-    /**
-     * @generated
-     */
     private String startDate;
-
-    /**
-     * @generated
-     */
     private String endDate;
-
-    /**
-     * @generated
-     */
     private float minimumGrade;
 
-
     /**
-     * @generated
+     * The submittables this class contains.
      */
     private List<Submittable> submittable;
 
+    public Course() {
 
-    /**
-     * @generated
-     */
-    private String getName() {
+    }
+
+
+    public String getName() {
         return this.name;
     }
 
-    /**
-     * @generated
-     */
-    private void setName(String name) {
+
+    public void setName(String name) {
         this.name = name;
     }
 
 
-    /**
-     * @generated
-     */
-    private String getSubject() {
+    public String getSubject() {
         return this.subject;
     }
 
-    /**
-     * @generated
-     */
-    private void setSubject(String subject) {
+
+    public void setSubject(String subject) {
         this.subject = subject;
     }
 
 
-    /**
-     * @generated
-     */
-    private String getStartDate() {
+    public String getStartDate() {
         return this.startDate;
     }
 
-    /**
-     * @generated
-     */
-    private void setStartDate(String startDate) {
+
+    public void setStartDate(String startDate) {
         this.startDate = startDate;
     }
 
 
-    /**
-     * @generated
-     */
-    private String getEndDate() {
+    public String getEndDate() {
         return this.endDate;
     }
 
-    /**
-     * @generated
-     */
-    private void setEndDate(String endDate) {
+
+    public void setEndDate(String endDate) {
         this.endDate = endDate;
     }
 
 
-    /**
-     * @generated
-     */
-    private float getMinimumGrade() {
+    public float getMinimumGrade() {
         return this.minimumGrade;
     }
 
-    /**
-     * @generated
-     */
-    private void setMinimumGrade(float minimumGrade) {
+
+    public void setMinimumGrade(float minimumGrade) {
         this.minimumGrade = minimumGrade;
     }
 
 
-    /**
-     * @generated
-     */
     public List<Submittable> getSubmittable() {
         if (this.submittable == null) {
             this.submittable = new ArrayList<Submittable>();
@@ -125,9 +81,7 @@ public class Course {
         return this.submittable;
     }
 
-    /**
-     * @generated
-     */
+
     public void setSubmittables(List<Submittable> submittable) {
         this.submittable = submittable;
     }
@@ -135,9 +89,7 @@ public class Course {
 
     //                          Operations                                  
 
-    /**
-     * @generated
-     */
+
     public float calculateGrade() {
         // TODO implement calculateGrade()
         return -1.0f;
--- app/src/main/java/com/yepdevelopment/failure/Domain/Submittable.java
+++ app/src/main/java/com/yepdevelopment/failure/Domain/Submittable.java
@@ -4,164 +4,100 @@ package com.yepdevelopment.failure.Domain;
  * @generated
  */
 public class Submittable {
-
-    /**
-     * @generated
-     */
+    private String id;
     private String name;
-
-    /**
-     * @generated
-     */
     private String description;
-
-    /**
-     * @generated
-     */
     private String assignDate;
-
-    /**
-     * @generated
-     */
     private String dueDate;
-
-    /**
-     * @generated
-     */
     private float weight;
-
-    /**
-     * @generated
-     */
     private float maxGrade;
-
-    /**
-     * @generated
-     */
     private float achievedGrade;
 
-
     /**
-     * @generated
+     * The course that is associated with this Submittable.
      */
     private Course course;
 
+    public Submittable() {
 
-    /**
-     * @generated
-     */
-    private String getName() {
+    }
+
+
+    public String getName() {
         return this.name;
     }
 
-    /**
-     * @generated
-     */
-    private void setName(String name) {
+
+    public void setName(String name) {
         this.name = name;
     }
 
 
-    /**
-     * @generated
-     */
-    private String getDescription() {
+    public String getDescription() {
         return this.description;
     }
 
-    /**
-     * @generated
-     */
-    private void setDescription(String description) {
+
+    public void setDescription(String description) {
         this.description = description;
     }
 
 
-    /**
-     * @generated
-     */
-    private String getAssignDate() {
+    public String getAssignDate() {
         return this.assignDate;
     }
 
-    /**
-     * @generated
-     */
-    private void setAssignDate(String assignDate) {
+
+    public void setAssignDate(String assignDate) {
         this.assignDate = assignDate;
     }
 
 
-    /**
-     * @generated
-     */
-    private String getDueDate() {
+    public String getDueDate() {
         return this.dueDate;
     }
 
-    /**
-     * @generated
-     */
-    private void setDueDate(String dueDate) {
+
+    public void setDueDate(String dueDate) {
         this.dueDate = dueDate;
     }
 
 
-    /**
-     * @generated
-     */
-    private float getWeight() {
+    public float getWeight() {
         return this.weight;
     }
 
-    /**
-     * @generated
-     */
-    private void setWeight(float weight) {
+
+    public void setWeight(float weight) {
         this.weight = weight;
     }
 
 
-    /**
-     * @generated
-     */
-    private float getMaxGrade() {
+    public float getMaxGrade() {
         return this.maxGrade;
     }
 
-    /**
-     * @generated
-     */
-    private void setMaxGrade(float maxGrade) {
+
+    public void setMaxGrade(float maxGrade) {
         this.maxGrade = maxGrade;
     }
 
 
-    /**
-     * @generated
-     */
-    private float getAchievedGrade() {
+    public float getAchievedGrade() {
         return this.achievedGrade;
     }
 
-    /**
-     * @generated
-     */
-    private void setAchievedGrade(float achievedGrade) {
+
+    public void setAchievedGrade(float achievedGrade) {
         this.achievedGrade = achievedGrade;
     }
 
 
-    /**
-     * @generated
-     */
     public Course getCourse() {
         return this.course;
     }
 
-    /**
-     * @generated
-     */
+
     public void setCourse(Course course) {
         this.course = course;
     }
