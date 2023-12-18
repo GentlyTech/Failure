f3634de86742fe849e71c0d9d12aa0a5639c5ec8
GentlyTech <andyyep@hotmail.com>
2023-12-18 18:16:28 -0500

    Add course list to MainViewModel

--- app/src/main/java/com/yepdevelopment/failure/ViewModels/MainViewModel.java
+++ app/src/main/java/com/yepdevelopment/failure/ViewModels/MainViewModel.java
@@ -1,7 +1,21 @@
 package com.yepdevelopment.failure.ViewModels;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class MainViewModel extends ViewModel {
+import com.yepdevelopment.failure.Database.Entities.Course;
 
+import java.util.List;
+
+public class MainViewModel extends ViewModel {
+    private final MutableLiveData<List<Course>> courses = new MutableLiveData<>();
+
+    public LiveData<List<Course>> getCourses() {
+        return this.courses;
+    }
+
+    public void setCourses(List<Course> newCourses) {
+        this.courses.setValue(newCourses);
+    }
 }
