5012d5f3507ae83092bb993d3d6bcbab53621bbe
GentlyTech <andyyep@hotmail.com>
2023-12-18 15:43:02 -0500

    Add MainViewModel reference

--- app/src/main/java/com/yepdevelopment/failure/Activities/MainActivity.java
+++ app/src/main/java/com/yepdevelopment/failure/Activities/MainActivity.java
@@ -4,17 +4,23 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentContainerView;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.yepdevelopment.failure.R;
+import com.yepdevelopment.failure.ViewModels.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
+    MainViewModel mainViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
     }
 
 }
