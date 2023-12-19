293f5dd14911e343e1228ff311e51882d772086e
GentlyTech <andyyep@hotmail.com>
2023-12-19 00:55:30 -0500

    Add getInstance() method for AppDatabase

--- app/src/main/java/com/yepdevelopment/failure/Database/AppDatabase.java
+++ app/src/main/java/com/yepdevelopment/failure/Database/AppDatabase.java
@@ -1,6 +1,9 @@
 package com.yepdevelopment.failure.Database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
@@ -13,6 +16,15 @@ import com.yepdevelopment.failure.Database.Entities.Submittable;
 @Database(entities = { Course.class, Submittable.class }, version = 1)
 @TypeConverters({ CourseConverter.class })
 public abstract class AppDatabase extends RoomDatabase {
+    private static AppDatabase instance;
+
     public abstract CourseDAO courseDao();
     public abstract SubmittableDAO submittableDao();
+
+    public static AppDatabase getInstance(Context applicationContext) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(applicationContext, AppDatabase.class, "FailureDB").build();
+        }
+        return instance;
+    }
 }
