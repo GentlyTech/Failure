045a3e8ccbba5aee8e86843fb4cf3a8d9e70e93f
GentlyTech <andyyep@hotmail.com>
2023-12-19 14:57:47 -0500

    Add AddSubmittableFragment

---
+++ app/src/main/java/com/yepdevelopment/failure/Fragments/AddSubmittableFragment.java
@@ -0,0 +1,35 @@
+package com.yepdevelopment.failure.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yepdevelopment.failure.R;
+import com.yepdevelopment.failure.databinding.FragmentAddSubmittableBinding;
+
+public class AddSubmittableFragment extends Fragment {
+    private FragmentAddSubmittableBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentAddSubmittableBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+    }
+}
