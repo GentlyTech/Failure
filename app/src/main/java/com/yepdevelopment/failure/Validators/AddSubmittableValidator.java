bcb03a8e5d31a6209c4bda3dce10d356b3b35f04
GentlyTech <andyyep@hotmail.com>
2023-12-19 16:22:00 -0500

    Implement validation methods

--- app/src/main/java/com/yepdevelopment/failure/Validators/AddSubmittableValidator.java
+++ app/src/main/java/com/yepdevelopment/failure/Validators/AddSubmittableValidator.java
@@ -1,11 +1,18 @@
 package com.yepdevelopment.failure.Validators;
 
 public class AddSubmittableValidator {
-    /**
+    /**isEmpty
      * This class should not be instantiated.
      */
     private AddSubmittableValidator() {
 
     }
 
+    public static boolean isSubmittableNameValid(String submittableName) {
+        return submittableName != null && !submittableName.isEmpty();
+    }
+
+    public static boolean isWeightValid(float weight) {
+        return weight >= 0.0f && weight <= 100.0f;
+    }
 }
