2d9d5ae20fc4d30d1848484430a5e3783f45fbe9
GentlyTech <andyyep@hotmail.com>
2023-12-19 02:23:30 -0500

    Add preliminary field validation (not yet integrated)

---
+++ app/src/main/java/com/yepdevelopment/failure/Validators/AddCourseValidator.java
@@ -0,0 +1,36 @@
+package com.yepdevelopment.failure.Validators;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class AddCourseValidator {
+    /**
+     * This class should not be instantiated.
+     */
+    private AddCourseValidator() {
+
+    }
+
+    public static boolean isCourseNameValid(String courseName) {
+        return courseName != null && !courseName.isEmpty();
+    }
+
+    public static boolean isCourseSubjectValid(String courseSubject) {
+        return courseSubject != null && !courseSubject.isEmpty();
+    }
+
+    public static boolean isDateValid(String date) {
+        if (date == null) return false;
+
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy/MM/dd", Locale.CANADA);
+        dateFormatter.setLenient(false);
+
+        try {
+            dateFormatter.parse(date);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+}
