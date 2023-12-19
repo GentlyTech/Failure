44753b1f90a3e99cf7e3eaeb4af7b09f85faca37
GentlyTech <andyyep@hotmail.com>
2023-12-19 02:48:22 -0500

    Begin setting up CourseCard

---
+++ app/src/main/java/com/yepdevelopment/failure/Adapters/CourseAdapter.java
@@ -0,0 +1,29 @@
+package com.yepdevelopment.failure.Adapters;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.yepdevelopment.failure.Database.Entities.Course;
+import com.yepdevelopment.failure.ViewHolders.CourseViewHolder;
+
+public class CourseAdapter extends RecyclerView.Adapter<CourseViewHolder> {
+
+
+    @NonNull
+    @Override
+    public CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CourseViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/yepdevelopment/failure/ViewHolders/CourseViewHolder.java
@@ -0,0 +1,12 @@
+package com.yepdevelopment.failure.ViewHolders;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CourseViewHolder extends RecyclerView.ViewHolder {
+    public CourseViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
