48b659e1c08e5b1fe0caf19e0895711b6dc5336e
GentlyTech <andyyep@hotmail.com>
2023-12-19 21:25:52 -0500

    Add menu for SubmittableInfoFragment

--- app/src/main/java/com/yepdevelopment/failure/Fragments/SubmittableInfoFragment.java
+++ app/src/main/java/com/yepdevelopment/failure/Fragments/SubmittableInfoFragment.java
@@ -1,21 +1,26 @@
 package com.yepdevelopment.failure.Fragments;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.MenuHost;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.yepdevelopment.failure.Database.AppDatabase;
 import com.yepdevelopment.failure.Database.Entities.Submittable;
 import com.yepdevelopment.failure.R;
+import com.yepdevelopment.failure.Utils.JavaRX.Async;
 import com.yepdevelopment.failure.ViewModels.Activities.MainViewModel;
 import com.yepdevelopment.failure.databinding.FragmentSubmittableInfoBinding;
 
@@ -46,7 +51,24 @@ public class SubmittableInfoFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        MenuHost menuHost = requireActivity();
+        menuHost.addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.course_options, menu);
+            }
 
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.courseOptionDelete) {
+                    // TODO implement submittable deletion here
+                    mainViewModel.setSelectedSubmittable(null);
+                    navController.popBackStack();
+                    return true;
+                }
+                return false;
+            }
+        }, getViewLifecycleOwner());
     }
 
     @Override
